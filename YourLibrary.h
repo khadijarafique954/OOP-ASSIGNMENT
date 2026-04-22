e7cfd7c9a6ff39c0bd6e35facfb0219a7d14f088
khadijarafique954 <khadijarafique954@gmail.com>
2026-04-22 21:12:47 +0500

    Create YourLibrary.h

---
+++ YourLibrary.h
@@ -0,0 +1,90 @@
+#ifndef YOUR_LIBRARY_H
+#define YOUR_LIBRARY_H
+
+#include <iostream>
+using namespace std;
+
+class Ride {
+private:
+    float distance;
+    float duration;
+    float fare;
+
+public:
+
+    // 🔹 Constructor Overloading
+    Ride() {
+        distance = 0;
+        duration = 0;
+        fare = 0;
+    }
+
+    Ride(float d, float t) {
+        distance = d;
+        duration = t;
+        fare = calculateFare();
+    }
+
+    Ride(const Ride &r) {
+        distance = r.distance;
+        duration = r.duration;
+        fare = r.fare;
+    }
+
+    // 🔹 Function Overloading (same name)
+    float calculateFare() {
+        fare = 50 + distance * 10 + duration * 2;
+        return fare;
+    }
+
+    float calculateFare(float perKmRate) {
+        fare = 50 + distance * perKmRate + duration * 2;
+        return fare;
+    }
+
+    float calculateFare(float perKmRate, float perMinRate) {
+        fare = 50 + distance * perKmRate + duration * perMinRate;
+        return fare;
+    }
+
+    // 🔹 Operator Overloading
+
+    // + → combine fares
+    Ride operator+(const Ride &r) {
+        Ride temp;
+        temp.fare = this->fare + r.fare;
+        return temp;
+    }
+
+    // - → fare difference
+    Ride operator-(const Ride &r) {
+        Ride temp;
+        temp.fare = this->fare - r.fare;
+        return temp;
+    }
+
+    // * → apply multiplier (surge pricing)
+    Ride operator*(float multiplier) {
+        Ride temp;
+        temp.fare = this->fare * multiplier;
+        return temp;
+    }
+
+    // == → compare fares
+    bool operator==(const Ride &r) {
+        return this->fare == r.fare;
+    }
+
+    // < → cheaper ride
+    bool operator<(const Ride &r) {
+        return this->fare < r.fare;
+    }
+
+    // << → display
+    friend ostream& operator<<(ostream &out, const Ride &r) {
+        out << "Ride Fare = " << r.fare;
+        return out;
+    }
+};
+
+#endif
